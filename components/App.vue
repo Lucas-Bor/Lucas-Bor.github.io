650468c1095fd092e05a83dc571739ffd55a470d
Kalmuroth <lucas.bordner@cloudartifact.com>
2021-11-26 13:41:10 +0100

    fuck off

--- components/App.vue
+++ components/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <main>
-      <h1 class="main-title">Arbre du mana vivant V0.5 (L'équilibrage de 4 heures du mat')</h1>
+      <h1 class="main-title">Arbre du mana vivant V0.6 (Ta gueule, c'est magique)</h1>
       <ul class="class-list">
         <class-list
           v-for="classType in data.classes"
