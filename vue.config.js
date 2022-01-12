cbfc7fc9894b4d478c147223ad8163ef94a1b7aa
kalmuroth <lucas.bordner@cloudartifact.com>
2022-01-12 09:35:20 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,3 @@ module.exports = {
   publicPath: '/manatree/'
 }
 
-//.json
-"devDependencies": {
-      "execa": "latest"
-}
-
-//.json
-"scripts": {
-     "deploy": "node scripts/gh-pages-deploy.js"
-}
